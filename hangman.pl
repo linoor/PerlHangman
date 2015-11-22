1723e8a177bbc080c97fdc8037d0af64683373b8
= <michal.pomaranski@gmail.com>
2015-11-22 18:02:40 +0100

    working correct letters

--- hangman.pl
+++ hangman.pl
@@ -7,9 +7,10 @@ use Pics;
 my ($miss, $guesses, $guess, @blanks, $tries, @guessed, @wordbank, $letter);
 
 sub get_random_word{
-	my @wordbank = @_;
+	my $ref = shift;
+	my @wordbank = @{$ref};
 	my $selected = rand(scalar(@wordbank));
-	return split(//, $wordbank[$selected]);
+	return $wordbank[$selected];
 }
 
 sub display_board{
@@ -19,7 +20,7 @@ sub display_board{
 	my @correct_letters = @{$correct_letters};
 	my $secret_word     = ${$secret};
 
-	print $secret_word;
+	print "DEBUG secret word: $secret_word";
 
 	print $hangman_pics[scalar(@missed_letters)];
 	print "\n";
@@ -49,10 +50,11 @@ sub display_board{
 		print $letter;
 		print " ";
 	}
+	print "\n";
 }
 
-sub getGuess{
-	my $alreadyGuessed = shift;
+sub get_guess{
+	my @already_guessed = shift;
 
 	while(1) {
 		print "Guess a letter:\n";
@@ -61,7 +63,7 @@ sub getGuess{
 		chomp($guess);
 		if (length($guess) != 1) {
 			print "Please enter a single letter.\n";
-		} elsif ($guess ~~ $alreadyGuessed) {
+		} elsif ($guess ~~ @already_guessed) {
 			print "You have already guesses that letter. Choose again.\n";
 		} elsif (!$guess ~~ split //, "qwertyuiopasdfghjklzxcvbnm") {
 			print "Please enter a letter.\n";
@@ -77,20 +79,66 @@ sub playAgain{
 	return $again =~ /^y/;
 }
 
-my @missed_letters = ("a", "b");
-my @correct_letters = ("m");
-my $secret_word = "loremipsum";
-
 @wordbank = qw(cubicle scramble deduction envelope century ridiculous);
-my @word = get_random_word(@wordbank);
 
 $miss = 0;
 $guesses = 0;
 $tries = 0;
 
 print "Welcome to hangman\n";
-print "Your word has "; print scalar(@word); print " guesses";
 
-display_board(\@hangmanpics, \@missed_letters, \@correct_letters, \$secret_word);
-getGuess(@missed_letters);
-print playAgain();
+my @missed_letters = ("");
+my @correct_letters = ("");
+my $secret_word = get_random_word(\@wordbank);
+my $game_is_done = ""; # false
+
+print "DEBUG chosen word: $secret_word";
+
+while (1) {
+	display_board(\@hangmanpics, \@missed_letters, \@correct_letters, \$secret_word);
+
+	my @tmp_array = ();
+	push @tmp_array, @missed_letters;
+	push @tmp_array, @correct_letters;
+	my $guess = get_guess(\@tmp_array);
+
+	print "DEBUG guess $guess";
+	if (index($secret_word, $guess) != -1) {
+		print "DEBUG i'm here\n";
+		push @correct_letters, $guess;
+
+		# check if the player has won
+		my $found_all_letters = 1;
+		for my $c ($secret_word) {
+			if (!($c ~~ @correct_letters)) {
+				$found_all_letters = "";
+				last;
+			}
+		}
+
+		if ($found_all_letters) {
+			print "Yes! The secret word is $secret_word! You have won!\n";
+			$game_is_done = 1;
+		}
+	} else {
+		push @missed_letters, $guess;
+
+		# check that the player has guessed too many times and lost
+		if (scalar(@missed_letters) == (scalar(@hangmanpics)-1)) {
+			display_board(\@hangmanpics, \@missed_letters, \@correct_letters, $secret_word);
+			print "You have run out of guesses!\n";
+			$game_is_done = 1;
+		}
+	}
+
+	if ($game_is_done) {
+		if (playAgain()) {
+			@missed_letters = ();
+			@correct_letters = ();
+			$game_is_done = "";
+			$secret_word = get_random_word(\@wordbank);
+		} else {
+			last;
+		}
+	}
+}
